8a47483be69c7f89726a244b7aee9b3bb570910f
Chris Matteson <chris.matteson@puppetlabs.com>
2015-03-23 18:18:12 -0700

    this never ends

--- lib/puppet/provider/dnsclient/windows.rb
+++ lib/puppet/provider/dnsclient/windows.rb
@@ -3,10 +3,10 @@
 
   servers = ["4.2.2.3", "8.8.8.8"]
   nameservershash = {}
-  purge = false
+  purge = true
 
   def create(nameservers, purge)
-    nameservershash = buildhash nameservers
+    nameservershash = buildhash
     updatehash = addservers(nameservers, nameservershash, purge)
     puts updatehash
     if updatehash.empty?
@@ -18,22 +18,27 @@
   end
 
   def destroy(nameservers)
-    nameservershash = buildhash nameservers
+    nameservershash = buildhash
     updatehash = removeservers(nameservers, nameservershash)
-    updatedns updatehash
+    if updatehash.empty?
+      puts 'return 0'
+      resetaddresses updatehash
+    else
+      puts updatehash
+      updatedns updatehash
+    end
   end
-#
-#  def exists?(nameservers,purge)
-#    buildhash (nameservers,purge)
+
+#  def exists?(nameservers, purge)
+#    nameservershash = buildhash
 #    if not buildhash.return == 0
 #      return 1
 #    else
 #      return 0
 #  end
 
-  def buildhash(nameservers)
+  def buildhash
     nameservershash = {}
-    puts nameservers
     interfaces = `powershell.exe "(Get-DNSClientServerAddress | select -expand InterfaceAlias | Get-Unique) -join ','"`
     interfaces = interfaces.chomp
     interfaces = interfaces.split(",").map { |a| a }
@@ -48,20 +53,30 @@
 
   def addservers(nameservers, nameservershash, purge=false)
     updatehash = {}
+    puts nameservershash.inspect
+    puts nameservers.inspect
     nameservers.each do |nameserver|
       interface = `powershell.exe "Find-NetRoute -RemoteIPAddress #{nameserver} | select -expand InterfaceAlias | Get-Unique"`
       interface = interface.chomp
       nameserversarray = (nameservershash[:"#{interface}"]).split(",").map { |a| a }
-      if not nameserversarray.include? nameserver	
+      if purge == false
+        if not nameserversarray.include? nameserver
+          if updatehash.has_key?(:"#{interface}")
+            updatehash[:"#{interface}"] << ",#{nameserver}"
+          else
+            if nameservershash.has_key?(:"#{interface}")
+              updatehash[:"#{interface}"] = nameservershash[:"#{interface}"]
+              updatehash[:"#{interface}"] << ",#{nameserver}"
+            else
+              updatehash[:"#{interface}"] = "#{nameserver}"
+            end
+          end
+        end
+      else
         if updatehash.has_key?(:"#{interface}")
           updatehash[:"#{interface}"] << ",#{nameserver}"
         else
-          if purge == false && nameservershash.has_key?(:"#{interface}")
-            updatehash[:"#{interface}"] = nameservershash[:"#{interface}"]
-            updatehash[:"#{interface}"] << ",#{nameserver}"
-          else
-            updatehash[:"#{interface}"] = "#{nameserver}"
-          end
+          updatehash[:"#{interface}"] = "#{nameserver}"
         end
       end
     end
@@ -71,16 +86,31 @@
   def removeservers(nameservers, nameservershash)
     updatehash = {}
     nameservers.each do |nameserver|
-      interface = `powershell.exe "Find-NetRoute -RemoteIPAddress #{nameserver} | select -expand InterfaceAlias | Get-Unique"`
-      interface = interface.chomp
-      nameserversarray = (nameservershash[:"#{interface}"]).split(",").map { |a| a }
-      if nameserversarray.include? nameserver
-        if updatehash.has_key?(:"#{interface}")
-          updateserversarray = (updatehash[:"#{interface}"]).delete("#{nameserver}")
-          updatehash[:"#{interface}"] = updateserversarray
-         else
-          updateserversarray = (nameservershash[:"#{interface}"]).delete("#{nameserver}")
-          updatehash[:"#{interface}"] = updateserversarray
+      nameservershash.each do |key, value|
+        nameserversarray = ("#{value}").split(",").map { |a| a }
+        puts nameserver
+        puts nameserversarray.inspect
+        if nameserversarray.include? nameserver
+          if updatehash.has_key?(:"#{key}")
+            puts 'updates has key'
+            puts updatehash[:"#{key}"].inspect
+            if (updatehash[:"#{key}"]).includes? ","
+              updateserversarray = (updatehash[:"#{key}"]).split(",").map { |a| a }
+            else
+              updateserversarray = updatehash[:"#{key}"]
+            end
+            puts updateserversarray.inspect
+            updateserversarray.delete("#{nameserver}")
+            puts updateserversarray.inspect
+            updatehash[:"#{key}"] = updateserversarray
+          else
+            puts 'updates doesnt have key'
+            updateserversarray = nameserversarray
+            puts updateserversarray.inspect
+            updateserversarray.delete("#{nameserver}")
+            puts updateserversarray.inspect
+            updatehash[:"#{key}"] = updateserversarray
+          end
         end
       end
     end
@@ -88,10 +118,17 @@
     updatehash
   end
 
+  def resetaddresses(updatehash)
+    updatehash.each do |key, value|
+      cmd = `powershell.exe Set-DNSClientServerAddress -InterfaceAlias "#{key} -ResetServerAddresses"`
+    end
+  end
+
   def updatedns(updatehash)
     updatehash.each do |key, value|
-      output = `powershell.exe Set-DNSClientServerAddress -InterfaceAlias "#{key} -ServerAddresses "#{value}`
-      return output
+      puts key
+      puts value
+      `powershell.exe "Set-DNSClientServerAddress -InterfaceAlias #{key} -ServerAddress #{value}"`
     end
   end
 
